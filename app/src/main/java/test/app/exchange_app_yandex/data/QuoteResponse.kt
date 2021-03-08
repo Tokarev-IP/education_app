af295b2b16a62bf439ae6848f79ceb4c73b242a1
Tokarev-IP <tokarevrab@gmail.com>
2021-03-08 22:03:04 +0300

    added data response classes

--- app/src/main/java/test/app/exchange_app_yandex/data/QuoteResponse.kt
+++ app/src/main/java/test/app/exchange_app_yandex/data/QuoteResponse.kt
@@ -1,4 +1,25 @@
 package test.app.exchange_app_yandex.data
 
-class QuoteResponse {
+import com.google.gson.annotations.SerializedName
+
+data class QuoteResponse(
+
+    @SerializedName("c")
+    val current: Float,
+
+    @SerializedName("h")
+    val high: Float,
+
+    @SerializedName("l")
+    val low: Float,
+
+    @SerializedName("o")
+    val open: Float,
+
+    @SerializedName("pc")
+    val previous_close: Float,
+
+    @SerializedName("t")
+    val t: Float
+) {
 }
--- app/src/main/java/test/app/exchange_app_yandex/data/StockProfileTwoResponse.kt
+++ app/src/main/java/test/app/exchange_app_yandex/data/StockProfileTwoResponse.kt
@@ -1,4 +1,18 @@
 package test.app.exchange_app_yandex.data
 
-class StockProfileTwoResponse {
+data class StockProfileTwoResponse(
+
+    val country: String,
+    val currency: String,
+    val exchange: String,
+    val ipo: String,
+    val marketCapitalization: Int,
+    val name: String,
+    val phone: String,
+    val shareOutstanding: Float,
+    val ticker: String,
+    val weburl: String,
+    val logo: String,
+    val finnhubIndustry: String
+) {
 }
